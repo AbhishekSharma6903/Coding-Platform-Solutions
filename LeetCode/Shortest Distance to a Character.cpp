30aaaae205bb34bb3b71743f6584051331c46a8e
ABHISHEK SHARMA <99632495+AbhishekSharma6903@users.noreply.github.com>
2023-03-15 23:57:44 +0530

    leetcode8

---
+++ LeetCode/Shortest Distance to a Character.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> shortestToChar(string s, char c) {
+        int n=s.size();
+        vector <int> ans(n,n);
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n; j++){
+                if(s[j]==c){
+                    ans[i]=min(ans[i],abs(i-j));
+                }
+            }
+        }
+        return ans;
+    }
+};
