4f4196ba6db964254903e07abeade4870e3cc4c3
ABHISHEK SHARMA <99632495+AbhishekSharma6903@users.noreply.github.com>
2023-03-17 00:26:26 +0530

    q8

---
+++ HackerRank/C++/Structs.cpp
@@ -0,0 +1,26 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+/*
+    add code for struct here.
+*/
+struct Student
+{
+    unsigned age{};
+    std::string first_name{};
+    std::string last_name{};
+    unsigned standard{};
+};
+
+int main() {
+    Student st;
+    
+    cin >> st.age >> st.first_name >> st.last_name >> st.standard;
+    cout << st.age << " " << st.first_name << " " << st.last_name << " " << st.standard;
+    
+    return 0;
+}
