d2020e11f11a0c83d6f7d94ae84ab43e677591e8
ABHISHEK SHARMA <99632495+AbhishekSharma6903@users.noreply.github.com>
2023-03-18 00:30:53 +0530

    updated leetcode11

--- LeetCode/Sort Array By Parity.cpp
+++ LeetCode/Sort Array By Parity.cpp
@@ -2,20 +2,38 @@ class Solution {
 public:
     vector<int> sortArrayByParity(vector<int>& nums) {
         int n = nums.size();
-        int me=n-1 , frnd=0;
-        vector <int> res(n);
-        
+        int me=0;
         for(int i=0; i<n; i++){
             if(nums[i]%2==0){
-                res[frnd]=nums[i];
-                frnd++;
+                swap(nums[i],nums[me]);
+                me++;
             }
-            else{
-                res[me]= nums[i];
-                me--;
-            }
+        
         }
-    return res;
-
+        return nums;
     }
 };
+
+
+//                                                            WAY - 2 (using 2 pointers)
+// class Solution {
+// public:
+//     vector<int> sortArrayByParity(vector<int>& nums) {
+//         int n = nums.size();
+//         int me=n-1 , frnd=0;
+//         vector <int> res(n);
+        
+//         for(int i=0; i<n; i++){
+//             if(nums[i]%2==0){
+//                 res[frnd]=nums[i];
+//                 frnd++;
+//             }
+//             else{
+//                 res[me]= nums[i];
+//                 me--;
+//             }
+//         }
+//     return res;
+
+//     }
+// };
