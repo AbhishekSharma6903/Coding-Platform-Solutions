c8f7069137098600992650f641cc73be307e6816
ABHISHEK SHARMA <99632495+AbhishekSharma6903@users.noreply.github.com>
2023-03-18 17:38:20 +0530

    updated leetcode12

--- LeetCode/Sort Colors.cpp
+++ LeetCode/Sort Colors.cpp
@@ -19,3 +19,30 @@ public:
        
     }
 };
+// ------------------------------------------ A P P R O A C H ----- 2 -------------------------------------------
+//     class Solution {
+// public:
+//     void sortColors(vector<int>& nums) {
+//         int count0=0,count1=0,count2=0;
+//         int n = nums.size();
+//         for(int i=0; i<n; i++){
+//             if(nums[i]==0) count0++;
+//             else if(nums[i]==1) count1++;
+//             else count2++;
+//         }
+//         int x=0;
+//         while(count0--){
+//             nums[x]=0;
+//             x++;
+//         }
+//         while(count1--){
+//             nums[x]=1;
+//             x++;
+//         }
+//         while(count2--){
+//             nums[x]=2;
+//             x++;
+//         }
+       
+//     }
+// };
