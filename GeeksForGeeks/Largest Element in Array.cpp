345644909a392f5f03b4bab73dfbc1966bd0bedb
ABHISHEK SHARMA <99632495+AbhishekSharma6903@users.noreply.github.com>
2023-03-25 23:32:59 +0530

    gfg1

---
+++ GeeksForGeeks/Largest Element in Array.cpp
@@ -0,0 +1,14 @@
+class Solution
+{
+public:
+    int largest(vector<int> &arr, int n)
+    {
+        int i;
+    int max = arr[0];
+    for (i = 1; i < n; i++)
+        if (arr[i] > max)
+            max = arr[i];
+ 
+    return max;
+    }
+};
