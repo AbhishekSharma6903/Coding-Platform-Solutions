80be5a6b87aa5c3b1851960c584d4b49962c638e
ABHISHEK SHARMA <99632495+AbhishekSharma6903@users.noreply.github.com>
2023-03-26 12:15:41 +0530

    gfg2

---
+++ GeeksForGeeks/Searching an element in a sorted array.cpp
@@ -0,0 +1,21 @@
+class Solution{
+    public:
+    // Function to find element in sorted array
+    // arr: input array
+    // N: size of array
+    // K: element to be searche
+    int searchInSorted(int arr[], int N, int K) 
+    { 
+    
+       // Your code here
+       for(int i=0;i<N;i++)
+       {
+           if(arr[i]==K)
+           {
+               return 1;
+           }
+       }
+       return -1;
+       
+    }
+};
