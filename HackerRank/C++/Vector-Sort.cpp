42635f1d5c59d22980fa6d469132b418c33ca6b4
ABHISHEK SHARMA <99632495+AbhishekSharma6903@users.noreply.github.com>
2023-04-19 23:59:19 +0530

    q11

--- HackerRank/C++/Vector-Sort.cpp
+++ HackerRank/C++/Vector-Sort.cpp
@@ -8,14 +8,14 @@ using namespace std;
 
 
 int main() {
-    int n;
-    cin>>n;
-    vector <int> vec(n);
-    for(int i=0; i<n; i++){
+    int a;
+    cin>>a;
+    vector <int> vec(a);
+    for(int i=0; i<a; i++){
         cin>>vec[i];
     }   
     sort(vec.begin(),vec.end());
-    for(int i=0; i<n; i++){
+    for(int i=0; i<a; i++){
         cout<<vec[i]<<" ";
     }  
     
