ca597f53b33ecfffa226e1f66dbdbea25efc9d37
ABHISHEK SHARMA <99632495+AbhishekSharma6903@users.noreply.github.com>
2023-07-20 00:17:27 +0530

    easy : Prefix sum + Hash_map

---
+++ Coding_Ninjas/Longest Subarray With Zero Sum.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+int getLongestZeroSumSubarrayLength(vector<int> &arr){
+	unordered_map <int, int> mp;
+	int sum=0;
+	int maxi =0;
+	for(int i=0; i<arr.size();i++){
+		sum += arr[i];
+		if(sum == 0){
+			maxi++;
+		}
+		else{
+			if(mp.find(sum) != mp.end()){
+				maxi = max(maxi, i-mp[sum]);
+			}
+			else {
+				mp[sum] = i;
+			}
+		}
+	}
+	return maxi;
+	
+}
