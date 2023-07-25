9722e6eb6a3d6576719316c2b199b22a067bda10
ABHISHEK SHARMA <99632495+AbhishekSharma6903@users.noreply.github.com>
2023-07-25 11:54:13 +0530

    optimal code

---
+++ Coding_Ninjas/Merge Two Sorted Arrays Without Extra Space.cpp
@@ -0,0 +1,24 @@
+//BRUTE FORCE SOLUTION --------------
+
+
+//OPTIMAL SOLUTION ------------------
+#include<vector>
+void mergeTwoSortedArraysWithoutExtraSpace(vector<long long> &a, vector<long long> &b){
+	int n = a.size();
+	int m = b.size();
+	int left = n-1;
+	int right = 0;
+	while(left>=0 && right<m){
+		if(a[left]>b[right]){
+			swap(a[left], b[right]);
+			left--;
+			right++;
+		}
+		else {
+			break;
+		}
+	}
+	sort(a.begin(), a.end());
+	sort(b.begin(), b.end());
+	
+}
