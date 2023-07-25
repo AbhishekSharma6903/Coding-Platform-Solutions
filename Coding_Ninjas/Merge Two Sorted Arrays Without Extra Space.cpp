9382c98a8be687ce2943d100fedb95a3a75c5db1
ABHISHEK SHARMA <99632495+AbhishekSharma6903@users.noreply.github.com>
2023-07-25 11:55:15 +0530

    Med : Merging of arrays + sorting

--- Coding_Ninjas/Merge Two Sorted Arrays Without Extra Space.cpp
+++ Coding_Ninjas/Merge Two Sorted Arrays Without Extra Space.cpp
@@ -1,6 +1,41 @@
 //BRUTE FORCE SOLUTION --------------
+#include<vector>
+void mergeTwoSortedArraysWithoutExtraSpace(vector<long long> &a, vector<long long> &b){
+	int n = a.size();
+	int m= b.size();
+	long long c[n+m];
+	int left =0; 
+	int right = 0 ;
+	int index =0;
+	while(left<n && right<m){
+		if(a[left]< b[right]){
+			c[index] = a[left];
+			index ++;
+			left++;
+		}
+		else {
+			c[index] = b[right];
+			index ++;
+			right++;
+		}
+	}
+	while(left<n){
+		c[index] = a[left];
+		index++, left++;
+	}
+	while(right<m){
+		c[index] = b[right];
+		index++, right++;
+	}
+        for (int i = 0; i < n + m; i++) {
+          if (i < n)
+            a[i] = c[i];
+          else
+            b[i - n] = c[i];
+        }
+}
 
-
+-----------------------------------------------------------------------
 //OPTIMAL SOLUTION ------------------
 #include<vector>
 void mergeTwoSortedArraysWithoutExtraSpace(vector<long long> &a, vector<long long> &b){
