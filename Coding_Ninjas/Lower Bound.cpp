39d942f3f7444be525ccc0525be66aee831b56c3
ABHISHEK SHARMA <99632495+AbhishekSharma6903@users.noreply.github.com>
2023-08-10 10:01:42 +0530

    Very easy

---
+++ Coding_Ninjas/Lower Bound.cpp
@@ -0,0 +1,19 @@
+int lowerBound(vector<int> arr, int n, int x) {
+	
+	int low=0;
+	int high= n-1;
+	int ans = n;
+
+	if(low>high) return -1;
+	while(low<=high){
+		int mid = (low+high)/2;
+		if(arr[mid]>=x){
+			ans= mid;
+			high = mid-1;
+		}
+		else {
+			low = mid+1;
+		}
+	}
+	return ans;
+}
