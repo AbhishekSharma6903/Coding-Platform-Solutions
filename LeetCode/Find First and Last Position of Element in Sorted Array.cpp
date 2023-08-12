0a7a9d885e52d689c063ef47cf0ebf207d1ec7ec
ABHISHEK SHARMA <99632495+AbhishekSharma6903@users.noreply.github.com>
2023-08-12 23:30:34 +0530

    linear seach

---
+++ LeetCode/Find First and Last Position of Element in Sorted Array.cpp
@@ -0,0 +1,19 @@
+//METHOD 1:  LINEAR SEARCH 
+class Solution {
+public:
+    vector<int> searchRange(vector<int>& nums, int target) {
+        int first =-1;
+        int last  =-1;
+
+        int n = nums.size();
+        for(int i=0;i<n; i++){
+            if(nums[i]==target){
+                if(first==-1){
+                    first = i;
+                }
+                last = i;
+            }
+        }
+        return {first, last};
+    }
+};
