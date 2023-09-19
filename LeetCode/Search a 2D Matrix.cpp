d95382a178c14e629487a7f4d586a04ad8405197
ABHISHEK SHARMA <99632495+AbhishekSharma6903@users.noreply.github.com>
2023-09-20 01:06:56 +0530

    Easy : Logic of NUMBER (1D) to COORDINATE (2D)

---
+++ LeetCode/Search a 2D Matrix.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& arr, int target) {
+        int n = arr.size();
+        int m = arr[0].size();
+        int low=0;
+        int high = n+m-1;
+        while(low<high){
+            int mid = (low + high) /2;
+            int row = mid / m;
+            int col = mid % m;
+            if(arr[row][col] == target) return true; 
+            else if (arr[row][col]< target) {
+                low = mid+1;
+            }
+            else high = mid- 1;
+        }
+        return false ;
+    }
+};
