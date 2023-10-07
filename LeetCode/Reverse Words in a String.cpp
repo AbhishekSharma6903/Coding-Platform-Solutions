5db730533ce21e96dc8895453a3ebd64435d439c
ABHISHEK SHARMA <99632495+AbhishekSharma6903@users.noreply.github.com>
2023-10-08 01:38:13 +0530

    Med : 2 logics reverse letters and then words

--- LeetCode/Reverse Words in a String.cpp
+++ LeetCode/Reverse Words in a String.cpp
@@ -26,3 +26,41 @@ public:
         return ans;
     }
 };
+
+
+--------------------------------------------------------------------------------------------------------------------
+    class Solution {
+public:
+
+    string removespaces(string s){
+        int n= s.size();
+        int i=0, j=0;
+        while(j<n){
+            if(s[j]!=' ' || (j!=0 && s[j-1]!=' ')){
+                s[i] = s[j];
+                i++;
+            } 
+            j++;
+        }
+        if(s[i-1]==' '){
+            i--;}
+        return s.substr(0,i);
+        }
+    
+
+    string reverseWords(string s) {
+        s = removespaces(s);
+        int n= s.size();
+        int end=0;
+        while(end<n){
+            int initial = end;
+            while(end<n && s[end]!=' '){
+                end++;
+            }
+            reverse(s.begin()+ initial, s.begin()+end);
+            end++;
+        }
+        reverse(s.begin(), s.end());
+        return s;
+    }
+};
