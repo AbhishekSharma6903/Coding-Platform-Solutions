d3f5359ee2ceef9c66fb3a5f09aec92a95591750
ABHISHEK SHARMA <99632495+AbhishekSharma6903@users.noreply.github.com>
2023-12-08 23:24:20 +0530

    Mid-easy : logic of dividing each divisions and then applying recursion

---
+++ Coding_Ninjas/Good Numbers.cpp
@@ -0,0 +1,23 @@
+bool isGood(int n, int digit, int sum){
+    // base case
+    if(n == 0){
+        return true;
+    }
+    int last = n % 10;
+    if((last > sum) && last != digit){
+        sum += last;
+        isGood(n / 10, digit, sum);
+    }else
+        return false;
+}
+
+ 
+vector<int> goodNumbers(int a, int b, int d) {
+    vector<int> ans;
+    for(int i=a; i<=b; i++){
+        if(i % 10 == d) continue;
+        if(isGood(i / 10, d, i % 10))
+            ans.push_back(i);
+    }
+    return ans;    
+}
