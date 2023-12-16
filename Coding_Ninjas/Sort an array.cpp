92b31de5d9aca7ca1cbfc3b3bb9e425b44785fc0
ABHISHEK SHARMA <99632495+AbhishekSharma6903@users.noreply.github.com>
2023-12-16 11:39:43 +0530

    using binary sort

---
+++ Coding_Ninjas/Sort an array.cpp
@@ -0,0 +1,25 @@
+void sortArray(vector<int> &arr, int n){
+    // Write your code here.
+    int low=0;
+    int mid =0;
+    int high =n-1;
+    while(mid<=high)
+    {
+        if(arr[mid]==0)
+        {
+            swap(arr[low], arr[mid]);
+            low++;
+            mid++;
+        }
+        else if(arr[mid]==1)
+        {
+            mid++;
+        }
+        else 
+        {
+            swap(arr[mid], arr[high]);
+                high--;
+        }
+    }
+    // return arr;
+}
