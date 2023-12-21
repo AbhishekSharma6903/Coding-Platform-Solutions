dee0ac1333e498156190d48bc687ce3a8a51744e
ABHISHEK SHARMA <99632495+AbhishekSharma6903@users.noreply.github.com>
2023-12-21 08:37:22 +0530

    multiple ways of solving

--- LeetCode/Two Sum.cpp
+++ LeetCode/Two Sum.cpp
@@ -1,3 +1,4 @@
+//Method 1: brute force approach O(n^2)
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
@@ -13,3 +14,58 @@ public:
         return {a,b};
     }
 };
+
+
+//Method 2: optimal approach O(n logn) : hash table (target-b =a -> must be array , since a+b=target)
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        int n = nums.size();
+        map <int,int> mpp;
+        for(int i=0; i<n; i++){
+            int req = target - nums[i];
+            if(mpp.find(req)!=mpp.end()){
+                return {mpp[req], i};
+            }
+            mpp[nums[i]] = i;
+        }
+        return {-1,-1};
+    }
+};
+
+//Method 1: Best approach O(n) : Two pointer appraoch
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        int n = nums.size();
+        vector<int> original(nums);
+        sort(nums.begin(), nums.end());
+        int first = 0;
+        int last = n-1;
+        while(first<last){
+            int sum = nums[first]+nums[last];
+            int index1=-1, index2=-1;
+            if(sum == target){
+                for(int i=0; i<n; i++){
+                    if(original[i] == nums[first] && index1 == -1){ //index is -1 to get first occurence 
+                        index1=i;
+                    }
+                    else if(original[i] == nums[last]){
+                        index2 = i;
+                    }
+                }
+                return {index1, index2};
+            }
+            else if(target>sum){
+                first++;
+                
+            }
+            else{
+                last--;
+            }
+        }
+        return {-1,-1};
+    }
+};
+
+
