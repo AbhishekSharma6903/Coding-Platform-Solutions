22bd53698466ed74591f842d161c1af983921564
ABHISHEK SHARMA <99632495+AbhishekSharma6903@users.noreply.github.com>
2023-12-22 00:56:42 +0530

    SLIDING Window using( 1. vector/map   and    2.set  )

--- LeetCode/Longest Substring Without Repeating Characters.cpp
+++ LeetCode/Longest Substring Without Repeating Characters.cpp
@@ -1,4 +1,28 @@
 //Sliding window using set
+
+//best approach using vector/map:
+#include<bits/stdc++.h>
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        vector< int> mpp(256,-1);
+        int right=0, left=0;
+        int len=0;
+        int n= s.size();
+            while(right<n){
+                if(mpp[s[right]]!=-1){
+                    left = max(left, mpp[s[right]]+1);
+                }
+                mpp[s[right]]=right;
+                len = max(len, right-left+1);
+                right++;
+            }
+        return len;
+        }
+    
+};
+
+//method 2 using sets
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
