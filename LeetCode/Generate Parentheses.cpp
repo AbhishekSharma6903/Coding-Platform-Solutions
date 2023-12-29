ff67b25f97f5ad4a8303d6dd795140904c6ea2ef
ABHISHEK SHARMA <99632495+AbhishekSharma6903@users.noreply.github.com>
2023-12-29 23:59:17 +0530

    recursion

---
+++ LeetCode/Generate Parentheses.cpp
@@ -0,0 +1,24 @@
+#include <vector>
+#include <string>
+
+class Solution {
+public:
+    void helper(int n, std::vector<std::string>& ans, std::string output, int open, int close) {
+        if (output.size() == n * 2) {
+            ans.push_back(output);
+            return;
+        }
+        if (open < n) {
+            helper(n, ans, output + "(", open + 1, close);
+        }
+        if (close < open) {
+            helper(n, ans, output + ")", open, close + 1);
+        }
+    }
+
+    std::vector<std::string> generateParenthesis(int n) {
+        std::vector<std::string> ans;
+        helper(n, ans, "", 0, 0);
+        return ans;
+    }
+};
