23a237b0ffe854bcf7a2ec6eddf863ed3525219c
ABHISHEK SHARMA <99632495+AbhishekSharma6903@users.noreply.github.com>
2024-01-11 12:29:48 +0530

    Mid: using switching the flags

--- LeetCode/Zigzag Conversion.cpp
+++ LeetCode/Zigzag Conversion.cpp
@@ -1,31 +1,26 @@
-
 class Solution {
 public:
-
     string convert(string s, int numRows) {
-    
-    if(numRows <= 1) return s;
-    vector<string>v(numRows, ""); 
-
-    int j = 0, dir = -1;
-
-    for(int i = 0; i < s.length(); i++)
-    {
-
-        if(j == numRows - 1 || j == 0) dir *= (-1); 
-		 
-        v[j] += s[i];
-
-        if(dir == 1) j++;
-
-        else j--;
-    }
-
-    string res;
-
-    for(auto &it : v) res += it; 
-
-    return res;
-
+        vector<string> ans(numRows);
+        if(numRows==1) return s;
+        int i=0;
+        bool flag = false;
+        for(auto it: s){
+            ans[i]+=it;
+            if(i==0 || i==numRows-1){
+                flag = !flag;
+            }
+            if(flag){
+                i++;
+            }
+            else{
+                i--;
+            }
+        }
+        string result = "";
+        for(auto it: ans){
+            result+= it;
+        }
+        return result;
     }
 };
