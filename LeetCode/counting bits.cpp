a573a13bca511b00c07e6390f6c01cc59de21210
ABHISHEK SHARMA <99632495+AbhishekSharma6903@users.noreply.github.com>
2024-02-20 00:59:36 +0530

    Updated counting bits.cpp

--- LeetCode/counting bits.cpp
+++ LeetCode/counting bits.cpp
@@ -1,3 +1,4 @@
+//Method 1
 class Solution {
 public:
     vector<int> countBits(int n) {
@@ -11,6 +12,20 @@ public:
                 num = num/2;
             }
             ans.push_back(sum);
+        }
+        return ans;
+    }
+};
+
+
+//Metod 2 (optimal)
+class Solution {
+public:
+    vector<int> countBits(int n) {
+        vector<int> ans(n+1);
+        ans[0] = 0;
+        for(int i=1; i<=n; i++){
+            ans[i] = ans[i/2] + i%2; 
         }
         return ans;
     }
