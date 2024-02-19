02df12208bd113d5df58f31f30c17f6bbc983152
ABHISHEK SHARMA <99632495+AbhishekSharma6903@users.noreply.github.com>
2024-02-19 21:20:04 +0530

    Updated Find the duplicate number.cpp

--- LeetCode/Find the duplicate number.cpp
+++ LeetCode/Find the duplicate number.cpp
@@ -1,3 +1,4 @@
+//METHOD : 1
 class Solution {
 public:
     int findDuplicate(vector<int>& arr) {
@@ -9,6 +10,28 @@ public:
         for(auto it: mpp){
             if(it.second>1){
                 return it.first;
+            }
+        }
+        return -1;
+    }
+};
+
+//METHOD 2
+class Solution {
+public:
+    int findDuplicate(vector<int>& arr) {
+        sort(arr.begin(), arr.end());
+        int i=0;
+        int n =arr.size();
+        int j=1;
+        while(j<n){
+            if(arr[i]==arr[j]){
+                return arr[i];
+                
+            }
+            else {
+                i++;
+                j++;
             }
         }
         return -1;
