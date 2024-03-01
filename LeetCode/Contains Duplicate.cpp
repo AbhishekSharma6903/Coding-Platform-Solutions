b1a0447522a66a24872f865f401332405787c75d
ABHISHEK SHARMA <99632495+AbhishekSharma6903@users.noreply.github.com>
2024-03-01 23:47:47 +0530

    Updated Contains Duplicate.cpp

--- LeetCode/Contains Duplicate.cpp
+++ LeetCode/Contains Duplicate.cpp
@@ -3,6 +3,7 @@ public:
     bool containsDuplicate(vector<int>& nums) {
         sort(nums.begin(),nums.end());
         bool flag=false;
+
         for(int i=0; i<nums.size()-1;i++){
             if(nums[i]==nums[i+1]){
                 return true;
