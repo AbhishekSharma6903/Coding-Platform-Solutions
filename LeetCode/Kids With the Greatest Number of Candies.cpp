e99438b6d50c8d08994a97881caf1bbee5db1f51
ABHISHEK SHARMA <99632495+AbhishekSharma6903@users.noreply.github.com>
2024-03-03 10:46:33 +0530

    simple logic

--- LeetCode/Kids With the Greatest Number of Candies.cpp
+++ LeetCode/Kids With the Greatest Number of Candies.cpp
@@ -1,23 +1,17 @@
 class Solution {
 public:
-    vector<bool> kidsWithCandies(vector<int>& candies, int extraCandies) {
-        
-        
-        int n = candies.size();
-        vector <bool> ans(n);
-        int largest = candies[0];
-        for(int i=0; i<n; i++){
-            if(candies[i]>largest){
-                largest= candies[i];
-            } 
+    vector<bool> kidsWithCandies(vector<int>& can, int k) {
+        int n = can.size();
+        vector<bool> ans;
+        int sum=0;
+        int maxi = INT_MIN;
+        for(int i=0; i<n;i++){
+            maxi= max(can[i],maxi);
         }
         for(int i=0; i<n; i++){
-            if(candies[i] + extraCandies >= largest){
-                ans[i]=true;
-            }
-            else{
-                ans[i]=false;
-            }
+            sum = can[i]+k;
+            if(sum>=maxi) ans.push_back(true);
+            else ans.push_back(false);
         }
         return ans;
     }
