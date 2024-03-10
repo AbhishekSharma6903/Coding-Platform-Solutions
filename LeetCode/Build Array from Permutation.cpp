25977ecc2228b0ca584e67785f45e5ac97ed0160
ABHISHEK SHARMA <99632495+AbhishekSharma6903@users.noreply.github.com>
2024-03-10 22:41:33 +0530

    easy

--- LeetCode/Build Array from Permutation.cpp
+++ LeetCode/Build Array from Permutation.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     vector<int> buildArray(vector<int>& nums) {
+        
         vector<int> ans;
         for(int i=0;i<nums.size();i++){
             ans.push_back(nums[nums[i]]);
