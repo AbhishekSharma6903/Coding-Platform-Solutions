1f8454db382e30b70d1da6b0411f38b43ccf9b62
ABHISHEK SHARMA <99632495+AbhishekSharma6903@users.noreply.github.com>
2024-03-14 01:42:35 +0530

    Two pointer approach

---
+++ LeetCode/Count Pairs Whose Sum is Less than Target.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int countPairs(vector<int>& nums, int target) {
+        sort(nums.begin(), nums.end());
+        int cnt=0;
+        int l=0;
+        int r = nums.size()-1;
+        while(l<r){
+            if(nums[l]+nums[r] < target){
+                cnt = cnt + (r-l);
+                l++;
+            }
+            else{
+                r--;
+            }
+        }
+        return cnt;
+    }
+};
