fa5067a30cbcf1a1ec7b6fe924894ec3b710b279
ABHISHEK SHARMA <99632495+AbhishekSharma6903@users.noreply.github.com>
2024-03-14 10:18:39 +0530

    using indexes, using hash map

--- LeetCode/How Many Numbers Are Smaller Than the Current Number.cpp
+++ LeetCode/How Many Numbers Are Smaller Than the Current Number.cpp
@@ -1,22 +1,19 @@
 class Solution {
 public:
     vector<int> smallerNumbersThanCurrent(vector<int>& nums) {
-        int last = nums.size()-1;
-       
-   vector<int>  p;
-      for(int i = 0 ; i<last+1 ; i++){
-        int count = last ;
-          for(int j=0;j<last+1;j++){
-           if(i==j){
-               continue;
-           }
-           if(nums[j]>=nums[i]){
-                   count--;
-            }
-
-            }
-           p.push_back(count);
-           }
-        return p ; 
+        map<int,int>mp;
+        int n = nums.size();
+        vector<int>snum = nums;
+        
+        sort(snum.begin(),snum.end());
+        for(int i=n-1;i>=0;i--)
+        {
+            mp[snum[i]]=i;
+        }
+        
+        for(int i=0;i<n;i++)
+            nums[i] = mp[nums[i]];
+        
+        return nums;
     }
 };
