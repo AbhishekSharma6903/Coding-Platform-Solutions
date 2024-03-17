ee8c1e5a6c1a16e4a7909c1d1dd0a62ab830c0e4
ABHISHEK SHARMA <99632495+AbhishekSharma6903@users.noreply.github.com>
2024-03-17 11:47:55 +0530

    Using the Insert( ) function

---
+++ LeetCode/Create Target Array in the given order.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    vector<int> createTargetArray(vector<int>& nums, vector<int>& index) {
+        vector<int> target(nums.size());
+        int i=0;
+        while(i<nums.size()){
+            target.insert(target.begin()+ index[i], nums[i]);
+            i++;
+        }
+        target.resize(nums.size());
+        return target;
+    }
+};
