74e14b98750b44147f4a13f8384a93fb11763d11
ABHISHEK SHARMA <99632495+AbhishekSharma6903@users.noreply.github.com>
2024-03-18 11:14:04 +0530

    Easy

---
+++ LeetCode/Decompress Run-Length Encoded List.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    vector<int> decompressRLElist(vector<int>& nums) {
+        vector<int> ans;
+        for(int i=0; i<nums.size(); i+=2){
+            int A = nums[i];
+            for(int j=0;j<A; j++){
+                ans.push_back(nums[i+1]);
+            }
+        }
+        return ans;
+    }
+};
