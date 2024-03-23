8dcf8292185ab90b0e97ae071ec0b0fec9062d7d
ABHISHEK SHARMA <99632495+AbhishekSharma6903@users.noreply.github.com>
2024-03-23 14:10:39 +0530

    easy

---
+++ LeetCode/Find First Palindromic String in the Array.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+bool checker(string arr){
+    int n = arr.size();
+    int flag=0;
+    for(int i=0; i<n/2; i++){
+        if(arr[i] != arr[n-i-1]){
+            return false;
+        }
+    }
+    return true;
+}
+
+    string firstPalindrome(vector<string>& words) {
+        int n = words.size();
+        for(auto strr : words){
+            if(checker(strr)){
+                return strr;
+            }
+        }
+        return "";
+    }
+};
