e0bd6b4aaf5dd75529a42c00d1c0fd82b83bb67e
ABHISHEK SHARMA <99632495+AbhishekSharma6903@users.noreply.github.com>
2024-03-24 14:23:33 +0530

    Easy

--- LeetCode/Sum of All Odd Length Subarrays.cpp
+++ LeetCode/Sum of All Odd Length Subarrays.cpp
@@ -1,18 +1,11 @@
 class Solution {
 public:
     int sumOddLengthSubarrays(vector<int>& arr) {
-        int n = arr.size() , ans =0;
-        for(int l=0;l<n;l++){
-            for(int r=l;r<n;r++){
-                if( ( r-l+1 ) % 2 ==1 ){
-                    int sum =0;
-                    for(int i = l;i<=r;i++){
-                        sum += arr[i];
-                    }
-                    ans += sum;
-                }
-            }
+        int sum(0), occurrence(0), n(arr.size());
+        for(int i = 0; i < n; i++){
+            occurrence = ceil(((float)(n - i) * (i + 1)) / 2);
+            sum += arr[i] * occurrence;
         }
-        return ans;
+        return sum;
     }
 };
