b3a4ccf2a994425f743771e2dff8e3c55e546598
ABHISHEK SHARMA <99632495+AbhishekSharma6903@users.noreply.github.com>
2024-04-26 22:48:05 +0530

    easy

---
+++ LeetCode/Apple Redistribution into Boxes.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int minimumBoxes(vector<int>& apple, vector<int>& capacity) {
+        int sum = 0;
+        for(int i=0; i<apple.size(); i++){
+            sum= sum+apple[i];
+        }
+        int count=0;
+        sort(capacity.begin(), capacity.end());
+        for(int i = capacity.size()-1; i>=0; i--){
+            sum = sum-capacity[i];
+            count++;
+            if(sum<=0) break;
+        }
+        return count;
+    }
+};
