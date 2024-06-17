85e643b3bf62857b089103e365a6eeaceab08e95
ABHISHEK SHARMA <99632495+AbhishekSharma6903@users.noreply.github.com>
2024-06-17 22:26:45 +0530

    left shift and right shift

--- GeeksForGeeks/Bit Manipulation.cpp
+++ GeeksForGeeks/Bit Manipulation.cpp
@@ -1,7 +1,7 @@
 class Solution{
 public:
     void bitManipulation(int num, int i) {
-        cout<<((num & (1<<i-1))>>i-1)<<" ";
+        cout<<((num & (1<<i-1))>>i-1)<<" "; // ((num >>i-1)&1)  -> Right shift
         cout<<(num | (1<<i-1))<<" ";
         cout<<(num&~(1<<i-1))<<" ";
     }
