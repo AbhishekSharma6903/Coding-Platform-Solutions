3b54595eba4949cd2e0718985538fed72503981a
ABHISHEK SHARMA <99632495+AbhishekSharma6903@users.noreply.github.com>
2024-06-23 23:06:16 +0530

    Have to review

---
+++ GeeksForGeeks/Count total set bits.cpp
@@ -0,0 +1,24 @@
+ int bit(int n) {
+    int prev = 1;
+    if (n == 0) return 1;
+    if (n == 1) return prev+1;
+    int curr;
+    for (int i = 1; i < n; i++) {
+        curr = (1 << i) + 2*prev; 
+        prev = curr;
+    }
+    return prev+1;
+}
+
+int countSetBits(int n) {
+    int ans = 0;
+    int cnt=0;
+    for (int i = 31; i >=0; i--) {
+        if (n & (1 << i)) {
+            ans += bit(i);
+            ans+=(1<<i)*cnt;
+            cnt++;
+        }
+    }
+    return ans;
+}
