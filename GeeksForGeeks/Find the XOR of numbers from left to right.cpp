b0a11e4bfc59561480359021c5312ed84fc276cc
ABHISHEK SHARMA <99632495+AbhishekSharma6903@users.noreply.github.com>
2024-06-25 22:21:18 +0530

    u have to the series

---
+++ GeeksForGeeks/Find the XOR of numbers from left to right.cpp
@@ -0,0 +1,13 @@
+int xoring(int x)
+    {
+        if(x % 4 == 1)return 1;
+        else if(x % 4 == 2)return x+1;
+        else if(x % 4 == 3)return 0;
+        else return x;
+    }
+    
+  
+    int findXOR(int l, int r) {
+        return xoring(r)^xoring(l-1);
+        // complete the function here
+    }
