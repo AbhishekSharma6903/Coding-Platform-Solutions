11af26f630cd1e0cdd1d1637a0c20857420203ef
ABHISHEK SHARMA <99632495+AbhishekSharma6903@users.noreply.github.com>
2024-06-30 11:01:58 +0530

    easy

---
+++ GeeksForGeeks/Implement stack using array.cpp
@@ -0,0 +1,12 @@
+void MyStack :: push(int x)
+{
+    top++;
+    arr[top]=x;
+}
+
+//Function to remove an item from top of the stack.
+int MyStack :: pop()
+{
+    if(top==-1) return -1;
+    else return arr[top--];
+}
