deacc19f0896cbedb06399e775fe5d881f912085
ABHISHEK SHARMA <99632495+AbhishekSharma6903@users.noreply.github.com>
2024-07-15 22:41:19 +0530

    Playing with Xor

--- LeetCode/Decode XORed Array.cpp
+++ LeetCode/Decode XORed Array.cpp
@@ -4,7 +4,6 @@ public:
         int n = encoded.size();
         vector<int> ans(n+1);
         ans[0] = first;
-
         for(int i=0; i<n; i++){
             ans[i+1]= (ans[i]^encoded[i]);
         }
